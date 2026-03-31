be649201f8d71fd7e18c4e54917f463f2bf3f315
Govardhan Reddy <govardhanreddykovvuri878@gmail.com>
2026-03-31 11:54:51 +0530

    React Task 01 Completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px", backgroundColor: "#1e293b", color: "white", padding: "30px", borderRadius: "12px", width: "400px", margin: "50px auto", boxShadow: "0 0 10px rgba(0,0,0,0.5)"}}>
+      <h1>My Introduction</h1>
+      <p><b>Name:</b> Kovvuri Govardhan Reddy</p>
+      <p><b>Email:</b> govardhanreddykovvuri@thestackly.com</p>
+      <p><b>Role:</b> Associate Software Engineer</p>
+      <p><b>About:</b> I am learning React and building web applications.</p>
+      </div>
+  );
+}
+export default App;
